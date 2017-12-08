c6f36a394735f549edddb0db3df6e4501894f593
humphrey09 <humphrey3009@gmail.com>
2017-12-08 23:10:13 +0100

    JSON-Funktion von Georg

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'process.json', true);
+    xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+    // .open will NOT return a value but simply returns
+    //undefined in async mode so use a callback
+    
+    callback(xobj.responseText);
+    }
+    }
+    xobj.send(null);
+    }
+    // Call to function with anonymous callback
+    loadJSON(function (response) {
+    var myJSON = JSON.parse(response);
+    console.log(myJSON);
+    });
     
